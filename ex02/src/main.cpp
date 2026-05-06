984976d36f0a0611f27c3ce5038a389746ace936
Janne Valkama <jvalkama@c1r5p12.hive.fi>
2026-05-06 17:05:30 +0300

    redundant comments

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,29 +6,9 @@
 /*   By: jvalkama <jvalkama@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/21 09:40:32 by jvalkama          #+#    #+#             */
-/*   Updated: 2026/05/06 17:03:21 by jvalkama         ###   ########.fr       */
+/*   Updated: 2026/05/06 17:05:14 by jvalkama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-/*
-Topic 3: Container Adapters and Protected Members (For Ex02: Mutated abomination)
-Exercise 02 requires you to make std::stack iterable by creating a MutantStack class. 
-By default, std::stack intentionally hides iterators to enforce LIFO (Last-In-First-Out) behavior.
-
-Concepts to Research:
-Container Adapters: Understanding how std::stack wraps an underlying container (like std::deque by default).
-The c Protected Member: std::stack has a protected member variable named c which is the actual 
-underlying container. Inheriting from std::stack allows you to access c.
-Type Aliasing (using vs. typedef): How to expose the iterators of the underlying container using 
-modern C++ using declarations (e.g., using iterator = typename std::stack<T>::container_type::iterator;).
-
-Resources:
-cppreference: std::stack https://en.cppreference.com/w/cpp/container/stack
-StackOverflow: How to iterate over a std::stack? 
-(This thread discusses the exact "hack" needed for this exercise). 
-https://stackoverflow.com/questions/525365/does-stdstack-expose-iterators
-LearnCpp: Type aliases https://www.learncpp.com/cpp-tutorial/typedefs-and-type-aliases/
-*/
 
 #include "MutantStack.h"
 #include <iostream>
